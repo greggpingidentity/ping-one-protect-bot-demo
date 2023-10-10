4e09f88cb03e47e499ed99bab0cc32c5dd7574bd
greggpingidentity <65311030+greggpingidentity@users.noreply.github.com>
2023-10-10 13:07:34 -0500

    Cleanup Bot Scripts

--- bot-scripts/dv-registration-bot.js
+++ bot-scripts/dv-registration-bot.js
@@ -1,70 +1,49 @@
 //PROTECT APP LOCATION
-//--Perform Bot Attack against shared glitch application
-const baseUrl = 'https://soaring-orchestrated-skies.glitch.me'
-//--Perform Bot Attack against local deployment of PingOne Protect Application
-// const baseUrl = 'http://localhost:3000'
+const baseUrl = 'http://bx.airlines:8888'
 
 //OS CONFIGURATION
-//--MAC-Chrome-Browser-Path
 const browserExecutablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-//--Windows-Chrome-Browser-Path
-// const browserExecutablePath = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
 
 //VISIBILITY
 const browserHiddenSetting = false
-// const browserHiddenSetting = true
 
-//const myCursor = require('ghost-cursor')
+//PUPPETEER SETUP
 const puppeteer = require('puppeteer-extra')
-//const {installMouseHelper} = require('./install-mouse-helper');
-
-// Add stealth plugin and use defaults 
 const pluginStealth = require('puppeteer-extra-plugin-stealth')
 const stealth = pluginStealth()
 const { executablePath } = require('puppeteer');
-
-//stealth.enabledEvasions.delete('user-agent-override')
 console.log(stealth.availableEvasions);
 puppeteer.use(stealth)
-
-// Stealth plugins are just regular `puppeteer-extra` plugins and can be added as such
 const UserAgentOverride = require('puppeteer-extra-plugin-stealth/evasions/user-agent-override')
-// Define custom UA and locale
 const ua = UserAgentOverride({
   userAgent: 'MyFunkyUA/1.0',
   locale: 'de-DE,de'
 })
 puppeteer.use(ua)
 
+//LAUNCH BOT
 puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-blink-features=AutomationControlled', '--disable-web-security', '--disable-xss-auditor'], executablePath: browserExecutablePath }).then(async browser => {
-  //browser new page
 
-  //browser new page
+  //New Browser
   const p = await browser.newPage();
-
   await p.setBypassCSP(true);
 
-  //launch URL
+  //GoTo URL
   await p.goto(baseUrl)
-  console.log("timeout started")
-  await p.waitForTimeout(2000)
-  console.log("timeout ended")
+  await p.waitForTimeout(1500)
 
-  //const cursor = myCursor.createCursor(p)
-  await p.click('#registerButton');
-  console.log("clicked registerButton");
-  await p.waitForTimeout(2000)
-
-  await p.waitForSelector('input[id="email"]');
-
-  // generate random email
+  //Generate Email
   var randomnumber = Math.floor(Math.random() * (99999 - 100 + 1)) + 100;
-  console.log(randomnumber)
-
   var randomemail = "ggg" + randomnumber + "@example.com";
   console.log(randomemail)
 
-  await p.waitForTimeout(3000)
+  //Select Registration Button
+  await p.click('#registerButton');
+  console.log("clicked registerButton");
+  await p.waitForSelector('input[id="email"]');
+
+  //Populate Registration Page with User Details
+  await p.waitForTimeout(1500)
   await p.type('input[id="email"]', randomemail);
   await p.waitForSelector('input[id="firstName"]');
   await p.type('input[id="firstName"]', 'GGG')
@@ -75,21 +54,9 @@ puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-blink-feat
   await p.waitForSelector('input[id="password"]');
   await p.type('input[id="password"]', 'botP@ssword1')
   console.log("put in password");
+  await p.waitForTimeout(1500)
   await p.click('#submitBtn');
   console.log("clicked submitRegistration");
-  await p.waitForTimeout(50000)
-  const n = await p.$("#access-token")
-
-  // if (n) {
-  //   const t = await (await n.getProperty('textContent')).jsonValue()
-  //   console.log(t);
-  //   console.log("SUCCESS WITH BOT")
-  // } else {
-  //   const findResult = await p.$("[class='reactSingularKey_messageTextArea2 styles_messageTextArea2__35NkK ']")
-  //   const text = await (await findResult.getProperty('textContent')).jsonValue()
-  //   console.log(text)
-  // }
-
+  await p.waitForTimeout(6000)
   await browser.close();
 })
-
--- bot-scripts/native-bot.js
+++ bot-scripts/native-bot.js
@@ -1,65 +1,59 @@
 //PROTECT APP LOCATION
-//--Perform Bot Attack against local deployment of PingOne Protect Application
-const baseUrl = 'https://localhost:8080/'
+const baseUrl = 'https://bx.finance:8080/'
 
 //OS CONFIGURATION
-//--MAC-Chrome-Browser-Path
 const browserExecutablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-//--Windows-Chrome-Browser-Path
-// const browserExecutablePath = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
 
 //VISIBILITY
 const browserHiddenSetting = false
-// const browserHiddenSetting = true
-const puppeteer = require('puppeteer-extra') 
  
-// Add stealth plugin and use defaults 
+//PUPPETEER SETUP
+const puppeteer = require('puppeteer-extra') 
 const pluginStealth = require('puppeteer-extra-plugin-stealth')
 const stealth = pluginStealth()
 const {executablePath} = require('puppeteer'); 
 console.log(stealth.availableEvasions);
 puppeteer.use(stealth)
-
-
-// Stealth plugins are just regular `puppeteer-extra` plugins and can be added as such
 const UserAgentOverride = require('puppeteer-extra-plugin-stealth/evasions/user-agent-override')
-// Define custom UA and locale
 const ua = UserAgentOverride({
   userAgent: 'MyFunkyUA/1.0',
   locale: 'de-DE,de'
 })
 puppeteer.use(ua)
 
+// LAUNCH BOT
 puppeteer.launch({ headless:browserHiddenSetting, ignoreHTTPSErrors:true, args: ['--no-sandbox', '--disable-blink-features=AutomationControlled', '--disable-web-security', '--disable-xss-auditor'], executablePath: browserExecutablePath}).then(async browser => {
 
-  //browser new page
+  //New Browser Page
    const p = await browser.newPage();
    await p.setBypassCSP(true);
 
-   //launch URL
+   //Launch URL
    await p.goto(baseUrl)
    console.log("timeout started")
    await p.waitForTimeout(2000)
    console.log("timeout ended")
 
-   // generate random email
+   //Generate Email
    var randomnumber = Math.floor(Math.random() * (99999 - 100 + 1)) + 100;
    console.log(randomnumber)
    var randomemail = "ggg" + randomnumber + "@example.com";
    console.log(randomemail)
+
+   //Populate Username Field
    await p.waitForSelector('input[id="username"]');
    await p.type('input[id="username"]',randomemail)
 
-   //put in password
+   //Populate Password Field
    await p.waitForSelector('input[id="password"]');
    await p.type('input[id="password"]','12345678!')
 
-   //click button
+   //Select Login Button
    await p.click('#authenticate');
    console.log('bot results');
-   
-   // show full Results
    await p.waitForTimeout(10000);
+   
+   //Show Full Results
    await p.click('#fullResults');
    console.log('full evaluation results');
    await p.waitForTimeout(10000);
@@ -67,4 +61,3 @@ puppeteer.launch({ headless:browserHiddenSetting, ignoreHTTPSErrors:true, args: 
    //close browser
    await browser.close();
 })
-
